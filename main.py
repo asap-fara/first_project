7f17d290adaab6050cc56b729d32e449dfe14f88
Farid Bagirov <zxcfara13@gmail.com>
2026-02-12 19:25:46 +0700

    create func summer

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print('Hello from repository!')
 print('ONE TWO THREE!!!!')
+
+def summer(nums):
+    result = 0 
+    for num in nums:
+        result += num 
+    return result 
+
+print(summer([1,2,3,4,5,6]))
