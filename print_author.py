c8e33af8ee390d3b9cdb1651eb8fa8229cee1cc7
Farid Bagirov <zxcfara13@gmail.com>
2026-02-14 00:01:19 +0700

    create print_author.py, del old files, upadte .gitignore

---
+++ print_author.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+import os
+
+def print_author():
+    load_dotenv('secret.env')
+    author = os.getenv('AUTHOR')
+    print(f'Автор проекта: {author}')
+
+print_author()
